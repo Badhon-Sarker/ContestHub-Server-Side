6e0f4cdf5811d59ba112279e10d1b4a61dc25055
Badhon-Sarker <badhonsarker38@gmail.com>
2024-06-16 04:57:49 +0600

    payment method complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const stripe = require('stripe')(process.env.VITE_STRIPE_SECRET)
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
@@ -298,7 +299,21 @@ const options = { upsert: true };
 
 
 
+app.post('/create-payment-intent', async(req, res)=>{
+  const {price} = req.body
+  const amount = parseInt(price * 100)
 
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card']
+  })
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+})
 
 
 
